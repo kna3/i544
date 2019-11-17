073a86cc23514e4b8be1839d6eb87b5d53715edd
kna3 <kna3@binghamton.edu>
2019-11-16 22:21:36 -0500

    created form for sensor types search

--- work/prj4-sol/sensors.js
+++ work/prj4-sol/sensors.js
@@ -13,57 +13,88 @@ const STATIC_DIR = 'statics';
 const TEMPLATES_DIR = 'templates';
 
 function serve(port, model, base='') {
-  const app = express();
-  app.locals.port = port;
-  app.locals.base = base;
-  app.locals.model = model;
-  process.chdir(__dirname);
-  app.use(base, express.static(STATIC_DIR));
-  setupTemplates(app);
-  setupRoutes(app);
-  app.listen(port, function() {
-      console.log(`listening on port ${port}`);
-  });
+    const app = express();
+    app.locals.port = port;
+    app.locals.base = base;
+    app.locals.model = model;
+    process.chdir(__dirname);
+    app.use(base, express.static(STATIC_DIR));
+    setupTemplates(app);
+    setupRoutes(app);
+    app.listen(port, function() {
+        console.log(`listening on port ${port}`);
+    });
 }
 
 
 module.exports = serve;
 
-
 function setupRoutes(app) {
-  const base = app.locals.base;
-  app.get(`${base}/sensor-types.html`, getSensorTypes(app));
+    const base = app.locals.base;
+    app.get(`${base}/sensor-types.html`, getSensorTypes(app));
 }
 
 function getSensorTypes(app) {
     return async function(req, res) {
-        console.log("sensor-types");
-        const model = { base: app.locals.base, fields: {}};
         const mustache = new Mustache();
-        const html = mustache.render('sensor-types', model);
+        const WIDGETS = [
+            {
+            name: 'id',
+            label: 'Sensor Type ID',
+            classes: [ 'tst-sensor-type-id'],
+            isRequired: false,
+            val: 'value 1',
+            errors: { input1: 'bad value error' }
+            },
+            {
+                name: 'modelNumber',
+                label: 'Model Number',
+                classes: [ 'tst-model-number'],
+                isRequired: false,
+                val: 'value 1',
+                errors: { input1: 'bad value error' }
+            },
+            {
+                name: 'manufacturer',
+                label: 'Manufacturer',
+                classes: [ 'tst-manufacturer'],
+                isRequired: false,
+                val: 'value 1',
+                errors: { input1: 'bad value error' }
+            },
+            { 
+            type: 'select',
+            name: 'quantity',
+            label: 'Measure',
+            choices: {
+                '': 'Select',
+                val1: 'Temperature',
+                val2: 'Pressure',
+                val3: 'Flow Rate',
+                val4: 'Relative Humidity'
+                    },
+            classes: [ 'tst-quantity'], 
+            val: 'val2',
+          },
+            {
+            type: 'submit',
+            name: ' ',
+            label: ' ',
+            val: 'search',
+            }];
+        let view = [];
+        for (const widget of WIDGETS) {
+            view.push(widgetView(widget, widget.val, widget.errors));
+        }
+        let partials = {form: view, render: function () {
+                return mustache.render('widget', this);
+            }};
+        let html = mustache.render('sensor-types', partials);
         res.send(html);
     }
+
 }
-
-/************************ General Utilities ****************************/
-
-/** Decode an error thrown by web services into an errors hash
- *  with a _ key.
- */
-function wsErrors(err) {
-    const msg = (err.message) ? err.message : 'web service error';
-    console.error(msg);
-    return { _: [ msg ] };
-}
-  
-function doMustache(app, templateId, view) {
-    const templates = { footer: app.templates.footer };
-    return Mustache.render(app.templates[templateId], view, templates);
-}
-
-
 function setupTemplates(app) {
-    console.log("template");
     app.templates = {};
     for (let fname of fs.readdirSync(TEMPLATES_DIR)) {
         const m = fname.match(/^([\w\-]+)\.ms$/);
@@ -78,5 +109,3 @@ function setupTemplates(app) {
         }
     }
 }
-
-
